eb7a4909201268b59cc95f2706a41e8c76a25bba
Manav Bhandari <manav.manav31@gmail.com>
2024-06-22 23:58:38 +0800

    Restructured project to match jinja format
    fixed the whole CSS and images stuff having error 404 thing by moving scss, css, img, js folders to a static folder next to templates folder cuz thats what jinja expects

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 
 @app.route("/")
-def hello():
+def index():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/img/<filename>')
+def send_image(filename):
+    return send_from_directory('static/img', filename)
 
+if __name__ == "__main__":
+    app.run(debug=True)
--- templates/README.md
+++
@@ -1,3 +0,0 @@
-# SecureGroup10
-
-Template Link: https://htmlcodex.com/vegetable-website-template

2cd7ba042e1b00edf34a9b079bf858608a09466c
SujanRaj20 <sujanraj3199@gmail.com>
2024-06-30 21:20:47 +0800

    Update email_utils.py

--- flask_app/email_utils.py
+++ flask_app/email_utils.py
@@ -5,17 +5,18 @@ mail = Mail()
 def init_mail(app):
     mail.init_app(app)
 
-    # Flask-Mail configuration
-    app.config['MAIL_SERVER'] = 'mail.smtp2go.com'
-    app.config['MAIL_PORT'] = 2525
-    app.config['MAIL_USERNAME'] = '2200975@sit.singaporetech.edu.sg'
-    app.config['MAIL_PASSWORD'] = 'cDoQYM9F624qF5mI'
+  # Flask-Mail configuration for Gmail
+    app.config['MAIL_SERVER'] = '***REMOVED***'
+    app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = False
+    app.config['MAIL_USERNAME'] = '***REMOVED***'
+    app.config['MAIL_PASSWORD'] = '***REMOVED***'  # Use your Gmail App Password here
+
 
 def send_otp_email(email, otp):
     with mail.connect() as conn:
-        msg = Message('Your OTP Code', sender='2200975@sit.singaporetech.edu.sg', recipients=[email])
+        msg = Message('Your OTP Code', sender='***REMOVED***', recipients=[email])
         msg.body = f'Your OTP code is {otp}. Please use this code to complete your login.'
         try:
             conn.send(msg)

9a9e0e0394b93df50c4409c52051b9e8a9f42726
Manav Bhandari <manav.manav31@gmail.com>
2024-07-07 02:54:14 +0800

    Create test_routes.py

---
+++ tests/test_routes.py
@@ -0,0 +1,11 @@
+import pytest
+from flask import url_for
+
+@pytest.mark.parametrize('endpoint', ['/', '/login', '/register'])
+def test_route_exists(client, endpoint):
+    response = client.get(endpoint)
+    assert response.status_code == 200
+
+def test_non_existing_route(client):
+    response = client.get('/non_existing_route')
+    assert response.status_code == 404

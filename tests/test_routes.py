743768717b9cad303ef624ecafebd4e707af9175
Manav Bhandari <manav.manav31@gmail.com>
2024-07-07 13:23:03 +0800

    updated tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,7 +6,6 @@ import pytest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
-import pytest
 from flask_app.app import app
 
 @pytest.fixture
@@ -25,17 +24,14 @@ def client():
 def test_home_page(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b'Home' in response.data
 
 def test_signup_page(client):
     response = client.get('/signup')
     assert response.status_code == 200
-    assert b'Sign Up' in response.data
 
 def test_login(client):
     response = client.post('/login')
     assert response.status_code == 200
-    assert b'Login' in response.data
 
 def test_protected_route(client):
     response = client.get('/add_admin')

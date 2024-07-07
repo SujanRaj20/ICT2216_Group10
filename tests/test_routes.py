69f524cececaebb5288f0881002039f53383a3ce
Manav Bhandari <manav.manav31@gmail.com>
2024-07-07 13:02:40 +0800

    updated unit tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,20 +1,33 @@
 import pytest
-from flask import url_for
+from flask_app import create_app
 
-# def test_true():
-#     assert True
-    
-# def test_false():
-#     assert False
-    
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config['TESTING'] = True
+    app.config['WTF_CSRF_ENABLED'] = False
 
-@pytest.mark.parametrize('endpoint', ['/'])
-def test_route_exists(client, endpoint):
-    response = client.get(endpoint)
+    with app.test_client() as client:
+        with app.app_context():
+            db.create_all()
+        yield client
+
+def test_home_page(client):
+    response = client.get('/')
     assert response.status_code == 200
+    assert b'Home' in response.data
 
-def test_non_existing_route(client):
-    response = client.get('/non_existing_route')
-    assert response.status_code == 404
-    
-    
+def test_signup_page(client):
+    response = client.get('/signup')
+    assert response.status_code == 200
+    assert b'Sign Up' in response.data
+
+def test_login(client):
+    response = client.post('/login')
+    assert response.status_code == 200
+    assert b'Login' in response.data
+
+def test_protected_route(client):
+    response = client.get('/add_admin')
+    assert response.status_code == 302
+    assert b'Location: /login' in response.headers['Location']

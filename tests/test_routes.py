302ce4c576eae62ba65f403a79b7aad7e1ba0ebd
Manav Bhandari <manav.manav31@gmail.com>
2024-07-07 12:50:26 +0800

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,18 +1,20 @@
 import pytest
 from flask import url_for
 
-def test_true():
-    assert True
+# def test_true():
+#     assert True
     
-def test_false():
-    assert False
+# def test_false():
+#     assert False
     
 
-# @pytest.mark.parametrize('endpoint', ['/'])
-# def test_route_exists(client, endpoint):
-#     response = client.get(endpoint)
-#     assert response.status_code == 200
+@pytest.mark.parametrize('endpoint', ['/'])
+def test_route_exists(client, endpoint):
+    response = client.get(endpoint)
+    assert response.status_code == 200
 
-# def test_non_existing_route(client):
-#     response = client.get('/non_existing_route')
-#     assert response.status_code == 404
+def test_non_existing_route(client):
+    response = client.get('/non_existing_route')
+    assert response.status_code == 404
+    
+    

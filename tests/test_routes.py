bc5beec5e87a1d125fff37c2dd5bbafa24d5a478
Manav Bhandari <manav.manav31@gmail.com>
2024-07-07 13:25:12 +0800

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,9 +2,8 @@ import sys
 import os
 import pytest
 
-# Add the parent directory to sys.path
+# Add the root directory to sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 
 from flask_app.app import app
 

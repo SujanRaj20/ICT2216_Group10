2bc5c718448c279c36bb52096df3027f70f43755
Manav Bhandari <manav.manav31@gmail.com>
2024-07-08 14:52:48 +0800

    Update logger.py
    updated logger.py to fix bug related to inconsistent log file name reading

--- flask_app/modules/logger.py
+++ flask_app/modules/logger.py
@@ -66,20 +66,22 @@ def configure_logging():                                        # Function to co
 def get_log_files(log_dir='logs'):                                  # Function to get a list of log files
     """Get a list of log files in the logs directory."""            
     current_app.logger.debug(f"get_log_files called")               # Log a debug message
-    return [f for f in os.listdir(log_dir) if f.endswith('.log')]   # Return a list of log files in the logs directory by filtering files that end with '.log' in the directory
+    return [f for f in os.listdir(log_dir) if f.startswith('app.')] # Return a list of log files in the logs directory by filtering files that end start with 'app.' in the directory
 
 def get_dates_with_logs(log_dir='logs'):                            # Function to get a list of dates for which log files are present
     """Get a list of dates for which log files are present."""
-    current_app.logger.debug(f"get_dates_with_logs called")         
+    current_app.logger.debug(f"get_dates_with_logs called")
     log_files = get_log_files(log_dir)                              # Get a list of log files in the logs directory by calling get_log_files
+    current_app.logger.debug(f"log_files: {log_files}")         
     
     # Check for 'app.log' and replace with today's date
-    dates = {f.split('.')[0] for f in log_files}                    # Get the date from the log file name by splitting the file name at the '.' and taking the first part
-    if 'app' in dates:                                              # If 'app' is in the dates
+    if 'app.log' in log_files:                                              # If 'app.log' is in the dates
         today = datetime.now(timezone('Asia/Singapore')).strftime("%Y-%m-%d")   # Get today's date in the Singapore timezone
-        dates.remove('app')                                         # Remove 'app' from the dates
-        dates.add(today)                                            # Replace it with today's date
+        log_files.remove('app.log')                                         # Remove 'app' from the dates
+        log_files.append('app.log.' + today)                                            # Replace it with today's date
     
+    dates = {f.split('.')[2] for f in log_files}                    # Get the date from the log file name by splitting the file name at the '.' and taking the first part
+                                                
     sorted_dates = sorted(dates)                                    # Sort the dates
     current_app.logger.debug(f"dates: {sorted_dates}")      
     return sorted_dates                                             # Return the sorted dates
@@ -91,16 +93,16 @@ def get_logs_within_date_range(start_date, end_date, log_dir='logs'):   # Functi
     today = datetime.now(timezone('Asia/Singapore')).strftime("%Y-%m-%d")   # Get today's date in the Singapore timezone
     if 'app.log' in log_files:                                              # If 'app.log' is in the log files
         log_files.remove('app.log')                                         # Remove 'app.log'
-        log_files.append(today + '.log')                                    # Add today's log file      
+        log_files.append('app.log.' + today)                                    # Add today's log file      
     
     selected_files = []                             # Initialize an empty list to store the selected files
     for log_file in log_files:                      # Iterate through the log files
-        log_date = log_file.split('.')[0]           # Get the date from the log file name by splitting the file name at the '.' and taking the first part
+        log_date = log_file.split('.')[2]           # Get the date from the log file name by splitting the file name at the '.' and taking the first part
         if start_date <= log_date <= end_date:      # Check if the log date is within the specified date range
             selected_files.append(log_file)         # Add the log file to the selected files
             
-    if today + '.log' in selected_files:            # If today's log file is in the selected files
-        selected_files.remove(today + '.log')       # Remove today's log file
+    if 'app.log.' + today in selected_files:            # If today's log file is in the selected files
+        selected_files.remove('app.log.' + today)       # Remove today's log file
         selected_files.append('app.log')            # Add 'app.log' to the selected files
     
     return selected_files                           # Return the selected files

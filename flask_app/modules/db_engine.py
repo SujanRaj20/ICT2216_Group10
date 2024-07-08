c8aba47f1e06be8c17b6e1dd9717cf981dcf7716
ljqnicole <nicxleleong@gmail.com>
2024-07-08 13:55:01 +0800

    load from.env

--- flask_app/modules/db_engine.py
+++ flask_app/modules/db_engine.py
@@ -1,4 +1,5 @@
 import logging
+from dotenv import load_dotenv
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, text, DECIMAL, JSON, DATE
 from sqlalchemy.sql import select
 from sqlalchemy.exc import SQLAlchemyError
@@ -10,13 +11,15 @@ import os
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)  # Set logging level to DEBUG
 
+# Load environment variables from .env file
+load_dotenv()
 
 # MySQL Server Parameters - Local
-local_mysql_host = os.getenv('MYSQL_HOST', 'mysql-container')
-local_mysql_port = 3306
-local_mysql_user = os.getenv('MYSQL_USER', 'bookwise_flask')
-local_mysql_password = os.getenv('MYSQL_PASSWORD', '***REMOVED***')
-local_mysql_db = os.getenv('MYSQL_DB', '***REMOVED***')
+local_mysql_host = os.getenv('MYSQL_HOST')
+local_mysql_port = os.getenv('MYSQL_PORT')
+local_mysql_user = os.getenv('MYSQL_USER')
+local_mysql_password = os.getenv('MYSQL_PASSWORD')
+local_mysql_db = os.getenv('MYSQL_DB')
 
 def get_engine():   # Function to return a MySQL engine when called
     engine = create_engine(f'mysql+pymysql://{local_mysql_user}:{local_mysql_password}@{local_mysql_host}:{local_mysql_port}/{local_mysql_db}')
